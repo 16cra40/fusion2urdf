71419fbb2ff7c7a9c73c9e922430d27054c64bd4
16cra40 <63052069+16cra40@users.noreply.github.com>
2025-05-24 18:21:57 +0100

    Remove outdated ROS Compatibility

--- URDF_Exporter/URDF_Exporter.py
+++ URDF_Exporter/URDF_Exporter.py
@@ -5,9 +5,6 @@
 
 import adsk, adsk.core, adsk.fusion, traceback
 import os
-import sys
-import tkinter as tk
-from tkinter import messagebox as mb
 from .utils import utils
 from .core import Link, Joint, Write
 
@@ -42,8 +39,6 @@ def run(context):
             return
 
         root = design.rootComponent  # root component 
-        components = design.allComponents
-
         # set the names
         robot_name = "default"      
         robot_name = ui.inputBox('Name of Robot', 
@@ -57,37 +52,11 @@ def run(context):
         if save_dir == False:
             ui.messageBox('Fusion2URDF was canceled', title)
             return 0
-        
-        appWin=tk.Tk()
-        appWin.title("Choose your ROS Version")
-        appWin.attributes('-toolwindow', True)
-        appWin.geometry('300x150')
 
-        ros_selection = tk.IntVar()
-        def sel():
-            appWin.destroy()
-            appWin.quit()
-
-
-        tk.Radiobutton(appWin, text="ROS 1",font=('Aerial', 14) ,indicatoron = 0, width = 150, height = 3, variable=ros_selection, value=1,
-                  command=sel).pack()
-
-        tk.Radiobutton(appWin, text="ROS 2",font=('Aerial', 14), indicatoron = 0, width = 150, height = 3, variable=ros_selection, value=2,
-                  command=sel).pack()
-
-        appWin.mainloop()
-
-        
-
-        
-        
-   
         save_dir= save_dir + '/' + package_name
         try: os.mkdir(save_dir)
         except: pass  
 
-
-        package_dir_ros1 = os.path.abspath(os.path.dirname(__file__)) + '/package_ros1/'
         package_dir_ros2 = os.path.abspath(os.path.dirname(__file__)) + '/package_ros2/'        
         # --------------------
         # set dictionaries
@@ -116,25 +85,13 @@ def run(context):
         links_xyz_dict = {} 
         # --------------------
         # Generate URDF
-        Write.write_urdf(joints_dict, links_xyz_dict, inertial_dict, material_dict, package_name, robot_name, save_dir, ros_selection.get() != 2)
+        Write.write_urdf(joints_dict, links_xyz_dict, inertial_dict, material_dict, package_name, robot_name, save_dir, 0)
         Write.write_materials_xacro(color_dict, robot_name, save_dir)
         Write.write_transmissions_xacro(joints_dict, links_xyz_dict, robot_name, save_dir)
-        if (ros_selection.get() == 2):
-
-            utils.copy_package(save_dir, package_dir_ros2)
-            utils.update_cmakelists(save_dir, package_name)
-            utils.update_package_xml(save_dir, package_name)
-            utils.update_ros2_launchfile(save_dir, robot_name)
-        else:
-            Write.write_gazebo_xacro(joints_dict, links_xyz_dict, inertial_dict, package_name, robot_name, save_dir)
-            Write.write_display_launch(package_name, robot_name, save_dir)
-            Write.write_gazebo_launch(package_name, robot_name, save_dir)
-            Write.write_control_launch(package_name, robot_name, save_dir, joints_dict)
-            Write.write_yaml(package_name, robot_name, save_dir, joints_dict)
- 
-            utils.copy_package(save_dir, package_dir_ros1)
-            utils.update_cmakelists(save_dir, package_name)
-            utils.update_package_xml(save_dir, package_name)
+        utils.copy_package(save_dir, package_dir_ros2)
+        utils.update_cmakelists(save_dir, package_name)
+        utils.update_package_xml(save_dir, package_name)
+        utils.update_ros2_launchfile(save_dir, robot_name)
 
         # Generate STl files        
         utils.export_stl(app, save_dir)   
